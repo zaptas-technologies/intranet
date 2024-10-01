a544c77aa56ba8904f2feded5b3fcae8970caa2c
Zaptas Technologies Pvt Ltd <90035005+zaptas-technologies@users.noreply.github.com>
2024-10-01 14:33:28 +0530

    added features

--- backend/index.js
+++ backend/index.js
@@ -2,18 +2,26 @@ const connect = require('./config/db');
 const express = require('express');
 const config = require('./config/connect');
 const announcementRoutes = require('./routes/announcementRoutes');
+const calendarRoutes = require('./routes/calendarRoutes');
 
 const app = express();
 
 // Middleware to parse JSON
 app.use(express.json());
 
-app.use('/v1/announcements', announcementRoutes); 
+// Define routes
+app.use('/v1/announcements', announcementRoutes);
+app.use('/v1/calendar', calendarRoutes);
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong!' });
+});
 
-const PORT = config.server.port   
+const PORT = config.server.port;
 
 app.listen(PORT, async () => {
-    await connect()
-  console.log(`Server is running on port ${PORT}`)
+  await connect();
+  console.log(`Server is running on port ${PORT}`);
 });
